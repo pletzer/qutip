1ee4d02d76a5a595ba6608c3b0a2dbb85590084a
Alex Pletzer <alexander@gokliya.net>
2023-12-20 22:09:13 +0000

    code fix, + message

--- qutip/_mkl/utilities.py
+++ qutip/_mkl/utilities.py
@@ -20,8 +20,9 @@ def _set_mkl():
         try:
             qset.mkl_lib = cdll.LoadLibrary(so_file)
             qset.has_mkl = True
+	    print(f'QuTip will use {so_file}')
             return
-        except
+        except:
             print(f'Could not open {so_file}')
 
     if (
